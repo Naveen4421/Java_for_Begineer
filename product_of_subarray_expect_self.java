1916124ee9a1b6dfd4c29ecf66f45b1b38d3ced5
Naveen S <168709832+Naveen4421@users.noreply.github.com>
2026-03-01 21:07:27 +0530

    Implement product of array except self method

---
+++ product_of_subarray_expect_self.java
@@ -0,0 +1,16 @@
+public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int[] answer=new int[n];
+        answer[0]=1;
+        for (int i=1;i<n;i++){
+            answer[i]=answer[i-1]*nums[i-1];
+        }
+        int right=1;
+        for (int j=n-1;j>=0;j--){
+            answer[j]*=right;
+            right*=nums[j];
+        }
+        return answer;
+        
+    }
+}
